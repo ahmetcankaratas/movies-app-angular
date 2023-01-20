df588e621830d681249b3a1b88a1a70755e48dea
ahmetcankaratas <karatascanahmet@yahoo.com>
2023-01-20 09:44:17 +0300

    add movies service

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  constructor(private http: HttpClient) {}
+
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=f18401a2d4366fa55a7a2d3876c9f14e');
+  }
+}
