00af6dbe4d705c4406f4f042fa7ad7165441b09c
ahmetcankaratas <karatascanahmet@yahoo.com>
2023-01-27 17:17:51 +0300

    add slider animation

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,11 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  animations: [trigger('slideFade', [state('void', style({ opacity: 0 })), transition('void <=> *', [animate('1s')])])]
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit{
   @Input() items: Movie[] = [];
+
+  currentSlideIndex: number = 0;
+
+  ngOnInit(){
+    setInterval(()=>{
+      this.currentSlideIndex = (this.currentSlideIndex + 1) % this.items.length;
+    },5000)
+  }
 }
