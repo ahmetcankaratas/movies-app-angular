c9108998d35463dada87222c69dd9969ae790654
ahmetcankaratas <karatascanahmet@yahoo.com>
2023-02-06 17:18:04 +0300

    add movie overview tab

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -9,16 +9,19 @@ import { Movie } from 'src/app/models/movie';
   styleUrls: ['./slider.component.scss'],
   animations: [trigger('slideFade', [state('void', style({ opacity: 0 })), transition('void <=> *', [animate('1s')])])]
 })
-export class SliderComponent implements OnInit{
+export class SliderComponent implements OnInit {
   @Input() items: Movie[] = [];
+  @Input() isBanner: boolean = false;
 
   currentSlideIndex: number = 0;
 
-  readonly imagesSizes = IMAGES_SIZES
+  readonly imagesSizes = IMAGES_SIZES;
 
-  ngOnInit(){
-    setInterval(()=>{
-      this.currentSlideIndex = (this.currentSlideIndex + 1) % this.items.length;
-    },5000)
+  ngOnInit() {
+    if (!this.isBanner) {
+      setInterval(() => {
+        this.currentSlideIndex = (this.currentSlideIndex + 1) % this.items.length;
+      }, 5000);
+    }
   }
 }
--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Movie } from 'src/app/models/movie';
-import { MoviesService } from 'src/app/services/movies.service';
-
+import { Movie } from '../../models/movie';
+import { MoviesService } from '../../services/movies.service';
+import { IMAGES_SIZES } from '../../constants/images-sizes';
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
@@ -10,6 +10,7 @@ import { MoviesService } from 'src/app/services/movies.service';
 })
 export class MovieComponent implements OnInit {
   movie: Movie | null = null;
+  imagesSizes = IMAGES_SIZES;
 
   constructor(private route: ActivatedRoute, private moviesService: MoviesService) {}
 
