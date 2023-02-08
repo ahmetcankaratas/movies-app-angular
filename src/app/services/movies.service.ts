353e91e9ed11e8783cdeaff860dd37061304cdd7
ahmetcankaratas <karatascanahmet@yahoo.com>
2023-02-08 14:39:34 +0300

    add search movies

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -21,12 +21,15 @@ export class MoviesService {
     );
   }
 
-  searchMovies(page: number) {
-    return this.http.get<MovieDto>(`${this.baseUrl}/movie/popular?page=${page}&api_key=${this.apiKey}`).pipe(
-      switchMap((response) => {
-        return of(response.results);
-      })
-    );
+  searchMovies(page: number, searchValue?: string) {
+    const uri = searchValue ? '/search/movie' : '/movie/popular';
+    return this.http
+      .get<MovieDto>(`${this.baseUrl}${uri}?page=${page}&query=${searchValue}&api_key=${this.apiKey}`)
+      .pipe(
+        switchMap((response) => {
+          return of(response.results);
+        })
+      );
   }
 
   getMovie(id: string) {
